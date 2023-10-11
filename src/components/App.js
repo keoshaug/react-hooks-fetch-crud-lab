53e27e02ec969901651e98cf3c3f95d8dcd03650
Kevin OShaughnessy <kevinoshaughnessy@ymail.com>
2023-10-10 23:26:12 -0400

    returning to this later

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,62 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+
+
+  const [questions, setQuestions] = useState([])
+
+  useEffect(()=>{
+    fetch('http://localhost:4000/questions')
+    .then(r => r.json())
+    .then(setQuestions)
+    }, [])
+
+    function handleSubmit(e) {
+      e.preventDefault()
+
+      const configOBj = {
+          method: "POST",
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body: {
+            "prompt": "",
+            "answers": [""],
+            // "correctIndex": integer
+        }
+      }
+
+      fetch('http://localhost:4000/questions', configOBj)
+      .then(r => r.json())
+      .then(questions =>setQuestions(questions))
+
+
+    }
+    
+    function handleViewQuestions(e) {
+      e.preventDefault()
+
+
+      const configOBj2 = {
+          method: "PATCH",
+          headers: {
+              "Content-Type": "application/json"
+          },
+          body: {
+            "prompt": "",
+            "answers": [""],
+            "correctIndex": 0
+        }
+      }
+
+      fetch('http://localhost:4000/questions', configOBj2)
+      .then(r => r.json())
+      .then(questions =>setQuestions(questions))
+  }
 
   return (
     <main>
